c91c7fe61d71573e4c7218d67aea73f18f89a9d7
ga <gabriel.machado.oli@gmail.com>
2026-07-30 20:37:53 -0300

    solução vergonhosa de um exercicio

--- c#/Estudos/Estudando/Program.cs
+++ c#/Estudos/Estudando/Program.cs
@@ -8,19 +8,105 @@ class Program
         Console.WriteLine("Tamanho da matriz:");
 
 
-        var matriz = CriarMatriz(int.Parse(Console.ReadLine()));
+         var matriz = CriarMatriz(int.Parse(Console.ReadLine()),
+                                     int.Parse(Console.ReadLine()));
         
+        
+
         matriz = PreencherMatriz(matriz);
 
-        PrintaDiagonalMatriz(matriz);  
+        // PrintaDiagonalMatriz(matriz);  
 
-        PrintaNumeroNegativo(matriz);     
+        // PrintaNumeroNegativo(matriz);   
+        System.Console.WriteLine("Escreva o item: ");
+        int item = int.Parse(Console.ReadLine());
+       
+        PrintaArredores(matriz,item);  
     }
-    
 
 
+    static void PrintaArredores(int[,] matriz,int item)
+    {
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                if(matriz[i,j] == item)
+                {
+                    Console.WriteLine("Position " + i +","+ j+":");
+                    Left(i,j,matriz);
+                    Right(i,j,matriz);
+                    Up(i,j,matriz);
+                    Down(i,j,matriz);
+                }
+            }
+        }
+    }
+    static void Up(int i, int j, int[,] matriz)
+    {
+        try
+        {
+            Console.WriteLine("Up: " + matriz[i-1,j]);
+            
+        }
+        catch (System.IndexOutOfRangeException)
+        {
+            
+            return;
+        }
+        
+    }
+
+    static void Down(int i, int j, int[,] matriz)
+    {
+        try
+        {
+            Console.WriteLine("Down: " + matriz[i+1,j]);
+            
+        }
+        catch (System.IndexOutOfRangeException)
+        {
+            
+            return;
+        }
+        
+    }
+
+    static void Left(int i, int j, int[,] matriz)
+    {
+        try
+        {
+            Console.WriteLine("Left: " + matriz[i,j-1]);
+            
+        }
+        catch (System.IndexOutOfRangeException)
+        {
+            
+            return;
+        }
+        
+    }
+
+    static void Right(int i, int j, int[,] matriz)
+    {
+        try
+        {
+            Console.WriteLine("Right: " + matriz[i,j + 1]);
+        }
+        catch (System.Exception)
+        {
+            
+            return;
+        }
+        
+    }
 
 
+    static int[,] CriarMatriz(int tamanhoLinha, int tamanhoCol)
+    {
+        int[,] matriz = new int[tamanhoLinha, tamanhoCol];
+        return matriz;
+    }
 
 
 
